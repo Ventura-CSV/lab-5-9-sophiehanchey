afa6d7ac6e3b59b24daed3ea22543e50a3e18576
Kyuwoong Lee <kwooler@gmail.com>
2023-06-29 19:06:30 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def main():
     numbers = [2, 3, 0, 5, 4]
     print(numbers)
     bubble(numbers)
-    print(numbers)
+    print(numbers)    # must be [2, 0, 3, 4, 5]
 
     numbers = [random.randint(0, 10) for i in range(10)]
     print(numbers)
